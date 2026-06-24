e874c2387ddb52e0c30a37d24410aaa83ad8fb72
udayempire <kuday0726@gmail.com>
2026-06-24 20:05:23 +0530

    chore: setup prisma , turborepo

---
+++ packages/database/src/client.ts
@@ -0,0 +1,22 @@
+
+// Source - https://stackoverflow.com/a/79805716
+// Posted by Amrita Pathak, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-06-24, License - CC BY-SA 4.0
+
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
